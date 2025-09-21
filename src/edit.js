62606891b537a82e3c6df453ac7eab41ce1b2f29
Seth Smigelski <51036602+SethSmigelski@users.noreply.github.com>
2025-09-21 13:46:00 -0700

    Update edit.js
    Change: Custom SVG icons. 
    NEW: Sidebar Button to edit settings

--- src/edit.js
+++ src/edit.js
@@ -2,8 +2,8 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, TextControl, ToggleControl, Button, Modal } from '@wordpress/components';
-import { Icon, cloud, mapMarker, cog, megaphone } from '@wordpress/components';
 import { useState } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
 
 export default function Edit({ attributes, setAttributes }) {
     const { locationName, latitude, longitude, unit } = attributes;
@@ -11,10 +11,9 @@ export default function Edit({ attributes, setAttributes }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-
+	const { openGeneralSidebar } = useDispatch('core/edit-post');
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
-
     const findCoordinates = () => {
         if (!searchTerm) return;
         setIsLoading(true);
@@ -27,6 +26,28 @@ export default function Edit({ attributes, setAttributes }) {
                 setIsLoading(false);
             });
     };
+
+	// Block Icons credits: Phosphor Icon Set - https://github.com/phosphor-icons/
+	const weatherIcon = (
+	  <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
+	    <path d="M164 68a80.4 80.4 0 0 0-18.46 2.15a60 60 0 0 0-6-7.42l7.57-10.82a12 12 0 0 0-19.66-13.77L119.87 49a59.9 59.9 0 0 0-22.26-5l-2.3-13a12 12 0 0 0-23.63 4.17l2.3 13a60 60 0 0 0-19.21 12.3l-10.86-7.61a12 12 0 0 0-13.77 19.66L41 80.11a59.5 59.5 0 0 0-5 22.25l-13 2.3a12 12 0 0 0 2.07 23.82a12.6 12.6 0 0 0 2.1-.18l13-2.3a59 59 0 0 0 3.44 7.25A56 56 0 0 0 84 228h80a80 80 0 0 0 0-160m-68 0a36 36 0 0 1 26.45 11.61a80.37 80.37 0 0 0-32.06 36.75A57 57 0 0 0 84 116a55.8 55.8 0 0 0-20.33 3.83A36 36 0 0 1 96 68m68 136H84a32 32 0 0 1 0-64h.28c-.11 1.1-.2 2.2-.26 3.3a12 12 0 0 0 24 1.4a56 56 0 0 1 1.74-11l.15-.55A56.06 56.06 0 1 1 164 204"/>
+	  </svg>
+	);
+	const degreesIcon = (
+	  <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
+	    <path d="M212 56a28 28 0 1 0 28 28a28 28 0 0 0-28-28m0 40a12 12 0 1 1 12-12a12 12 0 0 1-12 12m-84 57V88a8 8 0 0 0-16 0v65a32 32 0 1 0 16 0m-8 47a16 16 0 1 1 16-16a16 16 0 0 1-16 16m40-66V48a40 40 0 0 0-80 0v86a64 64 0 1 0 80 0m-40 98a48 48 0 0 1-27.42-87.4A8 8 0 0 0 96 138V48a24 24 0 0 1 48 0v90a8 8 0 0 0 3.42 6.56A48 48 0 0 1 120 232"/>
+	  </svg>
+	);
+	const locationIcon = (
+	  <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
+	    <path d="M128 60a44 44 0 1 0 44 44a44.05 44.05 0 0 0-44-44m0 64a20 20 0 1 1 20-20a20 20 0 0 1-20 20m0-112a92.1 92.1 0 0 0-92 92c0 77.36 81.64 135.4 85.12 137.83a12 12 0 0 0 13.76 0a259 259 0 0 0 42.18-39C205.15 170.57 220 136.37 220 104a92.1 92.1 0 0 0-92-92m31.3 174.71a249.4 249.4 0 0 1-31.3 30.18a249.4 249.4 0 0 1-31.3-30.18C80 167.37 60 137.31 60 104a68 68 0 0 1 136 0c0 33.31-20 63.37-36.7 82.71"/>
+	  </svg>
+	);
+	const gearIcon = (
+	  <svg width="20" height="20" viewBox="0 0 256 256" fill="currentColor">
+	    <path d="M128 76a52 52 0 1 0 52 52a52.06 52.06 0 0 0-52-52m0 80a28 28 0 1 1 28-28a28 28 0 0 1-28 28m92-27.21v-1.58l14-17.51a12 12 0 0 0 2.23-10.59A111.8 111.8 0 0 0 225 71.89a12 12 0 0 0-9.11-5.89l-22.28-2.5l-1.11-1.11L190 40.1a12 12 0 0 0-5.89-9.1a111.7 111.7 0 0 0-27.23-11.27A12 12 0 0 0 146.3 22l-17.51 14h-1.58L109.7 22a12 12 0 0 0-10.59-2.23a111.8 111.8 0 0 0-27.22 11.28A12 12 0 0 0 66 40.11l-2.5 22.28l-1.11 1.11L40.1 66a12 12 0 0 0-9.1 5.89a111.7 111.7 0 0 0-11.23 27.23A12 12 0 0 0 22 109.7l14 17.51v1.58L22 146.3a12 12 0 0 0-2.23 10.59a111.8 111.8 0 0 0 11.29 27.22a12 12 0 0 0 9.05 5.89l22.28 2.48l1.11 1.11L66 215.9a12 12 0 0 0 5.89 9.1a111.7 111.7 0 0 0 27.23 11.27A12 12 0 0 0 109.7 234l17.51-14h1.58l17.51 14a12 12 0 0 0 10.59 2.23A111.8 111.8 0 0 0 184.11 225a12 12 0 0 0 5.91-9.06l2.48-22.28l1.11-1.11L215.9 190a12 12 0 0 0 9.06-5.91a111.7 111.7 0 0 0 11.27-27.23A12 12 0 0 0 234 146.3Zm-24.12-4.89a70 70 0 0 1 0 8.2a12 12 0 0 0 2.61 8.22l12.84 16.05a86.5 86.5 0 0 1-4.33 10.49l-20.43 2.27a12 12 0 0 0-7.65 4a69 69 0 0 1-5.8 5.8a12 12 0 0 0-4 7.65L166.86 207a86.5 86.5 0 0 1-10.49 4.35l-16.05-12.85a12 12 0 0 0-7.5-2.62h-.72a70 70 0 0 1-8.2 0a12.06 12.06 0 0 0-8.22 2.6l-16.05 12.85A86.5 86.5 0 0 1 89.14 207l-2.27-20.43a12 12 0 0 0-4-7.65a69 69 0 0 1-5.8-5.8a12 12 0 0 0-7.65-4L49 166.86a86.5 86.5 0 0 1-4.35-10.49l12.84-16.05a12 12 0 0 0 2.61-8.22a70 70 0 0 1 0-8.2a12 12 0 0 0-2.61-8.22L44.67 99.63A86.5 86.5 0 0 1 49 89.14l20.43-2.27a12 12 0 0 0 7.65-4a69 69 0 0 1 5.8-5.8a12 12 0 0 0 4-7.65L89.14 49a86.5 86.5 0 0 1 10.49-4.35l16.05 12.85a12.06 12.06 0 0 0 8.22 2.6a70 70 0 0 1 8.2 0a12 12 0 0 0 8.22-2.6l16.05-12.85A86.5 86.5 0 0 1 166.86 49l2.27 20.43a12 12 0 0 0 4 7.65a69 69 0 0 1 5.8 5.8a12 12 0 0 0 7.65 4L207 89.14a86.5 86.5 0 0 1 4.35 10.49l-12.84 16.05a12 12 0 0 0-2.63 8.22"/>
+	  </svg>
+	);
 
     const selectLocation = (result) => {
         setAttributes({
@@ -95,22 +116,30 @@ export default function Edit({ attributes, setAttributes }) {
             )}
 
             <div {...useBlockProps()}>
-                <p><Icon icon={cloud} /> 
+                <p><Icon icon={weatherIcon} /> 
 					<strong>{__(' Under The Weather Forecast', 'under-the-weather')}</strong>
 				</p>
                 {locationName ? (
 				    <>
-						<p><Icon icon={mapMarker} />
+						<p><Icon icon={locationIcon} />
 							{` ${__('Location:', 'under-the-weather')} ${locationName}`}
 						</p>
-						<p><Icon icon={cog} />
+						<p><Icon icon={degreesIcon} />
 							{` ${__('Unit:', 'under-the-weather')} ${unit}`}
 						</p>
 					</>
                 ) : (
-					<p><Icon icon={megaphone} />
-                    	{` ${__('Please configure the weather widget in the block settings.', 'under-the-weather')}`}
-					</p>
+					    <div className="utw-placeholder">
+					        <p><Icon icon={gearIcon} />
+		                    	{` ${__('Please configure the weather widget in the block settings.', 'under-the-weather')}`}
+							</p>
+					        <Button 
+					            variant="primary" 
+					            onClick={() => openGeneralSidebar('edit-post/block')}
+					        >
+					            {__('Open Settings', 'under-the-weather')}
+					        </Button>
+					    </div>
                 )}
             </div>
         </>
